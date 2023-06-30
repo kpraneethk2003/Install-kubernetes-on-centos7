2fa7fee76f1b52a2b53e49376fc20937ccb7574c
kpraneethk2003 <112891540+kpraneethk2003@users.noreply.github.com>
2023-06-30 18:42:10 +0530

    Add files via upload

---
+++ calico.sh
@@ -0,0 +1,6 @@
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
+
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml
+
+watch kubectl get pods -n calico-system
+
